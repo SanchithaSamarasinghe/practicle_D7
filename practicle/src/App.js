ed85a325d6b4c1f9fb07496659af8fc89ec61111
SanchithaSamarasinghe <139639020+SanchithaSamarasinghe@users.noreply.github.com>
2024-11-24 12:03:42 +0530

    compiled

--- practicle/src/App.js
+++ practicle/src/App.js
@@ -4,14 +4,14 @@ import Child from './Child';
 import { useState} from 'react';
 
 function App() {
-  const[childname,setchildname]=useState("Default");
-  const reciveName=(name)=>{
-    setChiledname(name);
+  const[childname,setChildname]=useState("Default");
+  const receiveName=(name)=>{
+    setChildname(name);
   }
   return (
     <div className="App">
     <h2>Hello React I'm Parent</h2>
-    <Childcom setfun={receiveName}/>
+    <Child setfun={receiveName}/>
     <p>Parent:My child said:{childname}</p>
   </div>
   );
